8bd43ae46830a471ff9e4bf004ac4874d667e1eb
LAB27_USER ON WS22 <L27Y15W22@LabHCI.net>
2026-09-01 16:40:21 +0800

    updated urls.py

--- crms/students/urls.py
+++ crms/students/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.student_list),  # Adjust 'views.student_list' if your view function has a different name
+    path(
+        "",
+        views.student_list,
+        name="list",
+    ),
+
+
 ]
